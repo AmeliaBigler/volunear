5e0f94d318a0dabe4c8721c725dd6b2bc70b42b0
Amelia Bigler <ameliabigler@Amelias-Air.augiemagic.home>
2023-01-27 11:33:41 -0600

    fetch debug to dev branch

--- assets/display-search.js
+++ assets/display-search.js
@@ -9,4 +9,39 @@ $(function (){
         // TODO: create dynamic HTML elements for each result
 
     // TODO: call/invoke API function
+
+    var queryString = window.location.search;
+    var params = new URLSearchParams(queryString);
+    var proPubBaseURL = 'https://projects.propublica.org/nonprofits/api/v2/search.json';
+
+    var stateId = encodeURIComponent('state[id]');
+    var nteeId = encodeURIComponent('ntee[id]');
+
+    var city = encodeURIComponent(params.get('q'));
+    var state = params.get('state');
+    var category = params.get('category');
+
+    var proPubQueryString = '?q=' + city + '&' + stateId + '=' + state + '&' + nteeId + '=' + category;
+    var url = proPubBaseURL + proPubQueryString;
+
+    console.log(url);
+
+    fetch(url)
+        .then(function(response) {
+            console.log(response);
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error(
+                    response.status,
+                    response.statusText
+                );
+            }
+        }).then(function(data){
+            console.log(data);
+        }).catch(function(error){
+            console.log(error.message);
+        })
+
+
 })
